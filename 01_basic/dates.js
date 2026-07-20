1aa9df88a3de4cba49ed7d2200007eb75a74802e
Ayush katiyar <ayushkatiyar0908@gmail.com>
2026-07-20 10:31:21 +0000

    basic of array

---
+++ 01_basic/dates.js
@@ -0,0 +1,14 @@
+let mydates= new Date()
+// console.log(mydates.toString())
+// console.log(mydates.toDateString())
+// console.log(mydates.toLocaleString())
+// console.log(typeof mydates);
+
+let mycreatedate=new Date("01-04-2026")
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp)
+// console.log(mycreatedate.getTime());
+
+let newDate=new Date()
+console.log(newDate.toLocaleString())
---
+++ 02_basics/array.js
@@ -0,0 +1,23 @@
+//array
+const myarr=[0,1,2,3]
+// console.log(myarr[0])
+// myarr.push(4)
+// myarr.unshift(-1)
+// myarr.pop()
+// console.log(myarr)
+
+// console.log(myarr.includes(2));
+// console.log(myarr.indexOf(2));
+// console.log(myarr)
+
+// const myn1=myarr.slice(1,3)
+
+// console.log(myarr)
+// console.log(myn1)
+// const myn2=myarr.splice(1,4)
+// console.log(myn2)
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3));
