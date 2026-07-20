ce436e6ee52d257b0719711806ab74b8262ceb08
Ayush katiyar <ayushkatiyar0908@gmail.com>
2026-07-20 08:26:50 +0000

    chenges in string

---
+++ 01_basic/string.js
@@ -0,0 +1,7 @@
+const getname =new String("Ayush Katiyar");
+// console.log(getname.toUpperCase())
+// console.log(getname.charAt(3))
+//console.log(getname.indexOf('h'))
+console.log(getname.trim());
+
+console.log(getname.replace(" ","-"))
